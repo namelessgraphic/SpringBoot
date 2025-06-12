b825e665dbb6aabc34e2f17fa4a072f56581aedb
namelessgraphic <namelessdevgraphic@gmail.com>
2025-06-12 15:20:41 -0600

    add(Entities): Game entity

---
+++ src/main/java/com/example/project/model/Game.java
@@ -0,0 +1,51 @@
+package com.example.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "games")
+public class Game {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	private String company;
+	private String release_date;
+	private Integer classification;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getRelease_date() {
+		return release_date;
+	}
+	public void setRelease_date(String release_date) {
+		this.release_date = release_date;
+	}
+	public Integer getClassification() {
+		return classification;
+	}
+	public void setClassification(Integer classification) {
+		this.classification = classification;
+	}
+}
--- src/main/java/com/example/project/model/User.java
+++ src/main/java/com/example/project/model/User.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Table;
 @Table(name= "users")
 public class User {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String name;
